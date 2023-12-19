3d510fa761ca2391fbbcf3f06369dae5b16a6107
AbdelrahmanFawzyMesbah2 <128706363+AbdelrahmanFawzyMesbah2@users.noreply.github.com>
2023-12-19 13:24:38 +0200

    fixing Failed to execute 'removeChild' on 'Node': The node to be removed is not a child of this node

--- src/app/Admin/page.jsx
+++ src/app/Admin/page.jsx
@@ -1,4 +1,40 @@
 "use client";
+
+
+if (typeof Node === 'function' && Node.prototype) {
+  const originalRemoveChild = Node.prototype.removeChild;
+  Node.prototype.removeChild = function (child) {
+    if (child.parentNode !== this) {
+      if (console) {
+        console.warn('Cannot remove a child from a different parent', child, this);
+      }
+      return child;
+    }
+    return originalRemoveChild.apply(this, arguments);
+  };
+
+  const originalInsertBefore = Node.prototype.insertBefore;
+  Node.prototype.insertBefore = function (newNode, referenceNode) {
+    if (referenceNode && referenceNode.parentNode !== this) {
+      if (console) {
+        console.warn(
+          'Cannot insert before a reference node from a different parent',
+          referenceNode,
+          this
+        );
+      }
+      return newNode;
+    }
+    return originalInsertBefore.apply(this, arguments);
+  };
+}
+
+
+
+
+
+
+
 import "../../styles/globals.css";
 import React from "react";
 import ProfessorForm from "@/components/ProfessorForm";
@@ -7,11 +43,15 @@ import SectionForm from "@/components/SectionForm";
 export default function PageComponent() {
   return (
     <main className="flex-1 p-10">
+            <div key='uniqueKey'>
       <h1 className="text-3xl font-bold mb-6">Professor Information</h1>
+
+
       <ProfessorForm />
       <hr className="my-8" />
       <h1 className="text-3xl font-bold mb-6">Section Information</h1>
       <SectionForm />
+      </div>
     </main>
   );
 }
