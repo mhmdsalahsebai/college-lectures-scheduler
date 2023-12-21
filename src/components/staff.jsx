45e5e047aa7918a23f2152d90d27426b52b835be
AbdelrahmanFawzyMesbah2 <128706363+AbdelrahmanFawzyMesbah2@users.noreply.github.com>
2023-12-21 13:49:52 +0200

    Update staff.jsx

--- src/components/staff.jsx
+++ src/components/staff.jsx
@@ -1,9 +1,10 @@
 // Staff.js
 import React, { useState, useEffect } from 'react';
 
-const Staff = () => {
+const Staff = ({ onSelect }) => {
   const [data, setData] = useState([]);
   const [selectedItem, setSelectedItem] = useState('');
+  const [selectedStaff, setSelectedStaff] = useState("");
 
   useEffect(() => {
     // Fetch data from the JSON file
@@ -11,22 +12,34 @@ const Staff = () => {
       .then(response => response.json())
       .then(data => setData(data))
       .catch(error => console.error('Error fetching data:', error));
-  }, []);
+      setData([selectedItem]);
 
+  },[selectedItem]);
+
+
+  const handleSelectChange = (event) => {
+    // Update the state with the selected value
+    setSelectedStaff(event.target.value);
+  };
   return (
     <div>
-      <select
-        id="staffSelect"
-        value={selectedItem}
-        onChange={(e) => setSelectedItem(e.target.value)}
-      >
+<label>Choose a browser from this list:
+<input  list="browsers"
+          id="selectedStaff"
+          name="selectedStaff"
+          value={selectedStaff}
+          onChange={handleSelectChange} /></label>
+
+<datalist id="browsers">
+
         <option value="" disabled>Select from Staff</option>
         {data.map(item => (
-          <option key={item["Name"]} value={item["Name"]}>
+          <option key={item["id"]} value={item["Name"]}>
             {item["Name"]}
           </option>
         ))}
-      </select>
+</datalist>
+
     </div>
   );
 };
