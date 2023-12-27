d5e1c5349d9939594afc6b544167c84f5c521879
Mohamed Salah <mabdullah@cura.healthcare>
2023-12-27 22:24:43 +0200

    Add missing key props

--- src/app/Admin/page.jsx
+++ src/app/Admin/page.jsx
@@ -5,16 +5,13 @@ import InfoForm from "@/components/InfoForm";
 
 export default function Page() {
   const [selectedInstructor, setSelectedInstructor] = useState("Professor");
-  const list_Instructor = [
-    "Professor",
-    "Teaching Assistant",
-  ]
+  const list_Instructor = ["Professor", "Teaching Assistant"];
   return (
     <main className="flex-1 h-screen">
       <div className="flex pt-20 justify-center items-center bg-white dark:bg-gray-800 px-4 py-4 shadow-md w-full ">
         <ul className="flex  cursor-pointer ml-10 pd-10 pg-black">
           {list_Instructor.map((instructor) => (
-            <li className="mr-10">
+            <li key={instructor} className="mr-10">
               {instructor === selectedInstructor ? (
                 <a
                   className="text-green-200 border-b-2 border-green-500 pb-2"
@@ -34,10 +31,12 @@ export default function Page() {
           ))}
         </ul>
       </div>
-      <div >
-        <h1 className="flex  text-3xl font-bold mb-6 pt-6 justify-center ">{selectedInstructor} Form</h1>
+      <div>
+        <h1 className="flex  text-3xl font-bold mb-6 pt-6 justify-center ">
+          {selectedInstructor} Form
+        </h1>
         <InfoForm selectedInstructor={selectedInstructor} />
       </div>
-    </main >
+    </main>
   );
 }
--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,14 +1,13 @@
 import "../styles/globals.css";
 
 export const metadata = {
-  title: 'College Lectures Scheduler',
-}
+  title: "College Lectures Scheduler",
+};
 
 export default function RootLayout({ children }) {
-  
   return (
     <html lang="en">
       <body className="flex-1">{children}</body>
     </html>
-  )
+  );
 }
